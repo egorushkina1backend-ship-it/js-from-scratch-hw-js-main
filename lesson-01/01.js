ab11d66f37fbc1257dccb4067093666774ced218
Alex Egorushkina <egorushkina1backend@gmail.com>
2026-08-12 22:22:41 +0300

    Add homework files

---
+++ lesson-01/01.js
@@ -0,0 +1,11 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let count=0
+let sum =0
+
+
+while (count<20) {count= count+1
+    sum= sum+count
+    console.log(count+ ":"+ sum)
